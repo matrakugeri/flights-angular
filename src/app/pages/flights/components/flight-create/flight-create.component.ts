c07309324037f4f850a367fda84baf5d591a8ba7
matrakugeri <matrakugeri@gmail.com>
2025-04-21 00:25:34 +0200

    implemented create flight page design

--- src/app/pages/flights/components/flight-create/flight-create.component.ts
+++ src/app/pages/flights/components/flight-create/flight-create.component.ts
@@ -1,11 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatTimepickerModule } from '@angular/material/timepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-flight-create',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatTimepickerModule,
+    MatDatepickerModule,
+    MatSelectModule,
+  ],
+  providers: [provideNativeDateAdapter()],
   templateUrl: './flight-create.component.html',
   styleUrl: './flight-create.component.scss',
 })
